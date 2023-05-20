6da9dd72d62cec4633e76014bdd68c8c43227531
Thomas Gerlach <t.g.g@web.de>
2023-05-20 14:38:43 +0100

    add(sample toggler)

--- wir-schiffen-das/apps/client/src/app/app.component.ts
+++ wir-schiffen-das/apps/client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, MatSlideToggleModule],
   selector: 'wir-schiffen-das-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
