befbef1f71b60469f0c15f71890fdeef5a4bafdd
Thomas Gerlach <t.g.g@web.de>
2023-05-20 14:15:14 +0100

    removed(old angular)

--- wir-schiffen-das/apps/client-e2e/src/e2e/app.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('client', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('wir-schiffen-das-root').should('exist');
-  });
-});
--- wir-schiffen-das/apps/client-e2e/src/e2e/nx-welcome.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('client', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=nxwelcomecomponent--primary'));
-  it('should render the component', () => {
-    cy.get('wir-schiffen-das-nx-welcome').should('exist');
-  });
-});
