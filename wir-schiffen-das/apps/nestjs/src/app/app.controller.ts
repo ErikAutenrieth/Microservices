96343ea815fcf9bc2ca3d3cad94bc344d01159f3
Thomas Gerlach <t.g.g@web.de>
2023-05-20 14:03:26 +0100

    moved(to nestjs)

---
+++ wir-schiffen-das/apps/nestjs/src/app/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import { Customer } from '@wir-schiffen-das/types';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getData() {
+    return this.appService.getData();
+  }
+
+  @Get('hello')
+  getHello() {
+    const cost = new Customer('John Doe', 'email');
+    return cost.toString();
+  }
+}
---
+++ wir-schiffen-das/libs/types/src/lib/types.ts
@@ -0,0 +1,26 @@
+export function types(): string {
+  return 'types';
+}
+
+export class Customer {
+  private name: string;
+  private email: string;
+  
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+  
+  getName(): string {
+    return this.name;
+  }
+  
+  getEmail(): string {
+    return this.email;
+  }
+
+  toString(): string {
+    return this.name + ' (' + this.email + ')';
+  }
+}
+
