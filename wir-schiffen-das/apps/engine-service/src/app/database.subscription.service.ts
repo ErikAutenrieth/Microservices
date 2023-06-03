b54c1cd1796a3ff37aee8fd614b56b37fb62d8ac
Thomas Gerlach <thomas.gerlach@smail.inf.h-brs.de>
2023-06-03 14:18:02 +0100

    try(new package)

--- wir-schiffen-das/apps/engine-service/src/app/database.subscription.service.ts
+++ wir-schiffen-das/apps/engine-service/src/app/database.subscription.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { AlgorithmState, AlgorithmStateDocument } from '@wir-schiffen-das/nestjs-types';
+import { AlgorithmStateEnum } from '@wir-schiffen-das/types';
 import { Model } from 'mongoose';
-
+import { BehaviorSubject, Observable, filter, fromEvent, tap } from 'rxjs';
 @Injectable()
 export class DatabaseSubscriptionService {
 
+    behaviorSubject = new BehaviorSubject<AlgorithmStateDocument>(null);
+    
+    private stateChangeObservable = fromEvent(
+        this.algorithmState.watch(),
+        'change',
+      );
+    
+    
+    
+
     constructor(@InjectModel(AlgorithmState.name) private algorithmState: Model<AlgorithmStateDocument>) {
 
-        this.algorithmState.watch().on('change', this.handleStateChange);
+        this.algorithmState.watch().on('change', this.handleStartAlgorithmChange);
+        // to rxjs
+
+
+        this.stateChangeObservable
+        .pipe( tap(change => console.log('change', change) ),
+            filter(data => 
+                (data["ResultState"] !== AlgorithmStateEnum.ready && 
+                data["ResultState"] !== AlgorithmStateEnum.failed) &&
+                data["engineState"] === AlgorithmStateEnum.notStarted
+            ) )
+            .subscribe(change => this.handleStartAlgorithmChange(change));
     }
 
 
-    private handleStateChange(change) {
+    private handleStartAlgorithmChange(change) {
         const data = change.fullDocument;
         console.log('data', data);
         console.log('userID', data.userId);
