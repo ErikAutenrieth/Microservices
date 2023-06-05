fd80e43ad345c448fd7d476ac36e32c541ef81ed
Thomas Gerlach <thomas.gerlach@smail.inf.h-brs.de>
2023-06-05 11:01:40 +0100

    add(non tested CircuitBreaker)

--- wir-schiffen-das/apps/nestjs/src/app/engine/engine.service.ts
+++ wir-schiffen-das/apps/nestjs/src/app/engine/engine.service.ts
@@ -1,15 +1,25 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import CircuitBreaker from 'opossum';
 import { AlgorithmState, AlgorithmStateDocument } from '@wir-schiffen-das/nestjs-types';
 import { CreateAlgorithmStateDto, MicroserviceAddressEnum, InitializeAlgorithmMicroserviceDto } from '@wir-schiffen-das/types';
 import { Model } from 'mongoose';
+import { Axios } from 'axios';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class EngineService {
 
     constructor( @InjectModel(AlgorithmState.name) private algorithmState: Model<AlgorithmStateDocument>, private httpService: HttpService) {
+
     }
+
+    circuitBreakerOptions = {
+      timeout: 500,
+      errorThresholdPercentage: 50,
+      resetTimeout: 5000,
+    };
 
     async create(createAlgorithmStateDto: CreateAlgorithmStateDto): Promise<AlgorithmStateDocument> {
         const createdAlgorithmState = new this.algorithmState(createAlgorithmStateDto);
@@ -19,8 +29,12 @@ export class EngineService {
 
     async sendConfigurationToService(initializeAlgorithmMicroserviceDto: InitializeAlgorithmMicroserviceDto, microserviceAddressEnum: MicroserviceAddressEnum) {
 
-      //TODO add interceptor
-      return this.httpService.post(microserviceAddressEnum + "CheckConfiguration", initializeAlgorithmMicroserviceDto)
+      const apiCall = () => firstValueFrom(this.httpService.post( microserviceAddressEnum + "CheckConfiguration", initializeAlgorithmMicroserviceDto ));
+
+      const breaker = new CircuitBreaker(apiCall, this.circuitBreakerOptions);
+      breaker.fire()
+        .then(console.log)
+        .catch(console.error);
 
     }
  
